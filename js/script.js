bf21eec84ae6687bbce8571433b581491a329b58
JeffreyBoehme <jeffrey.boehme2@gmail.com>
2021-08-14 11:05:35 +1000

    Javascript updates for filled tags

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+
+$(".bi-hand-thumbs-up").on("click", function(){
+    $(this).toggleClass("bi-hand-thumbs-up");
+    $(this).toggleClass("bi-hand-thumbs-up-fill");
+    });
+
+$(".bi-bookmark-plus").on("click", function(){
+    $(this).toggleClass("bi-bookmark-plus");
+    $(this).toggleClass("bi-bookmark-plus-fill");
+    });
+
+$(".num58thumb").on("click", function(){
+    $(".num58").toggleClass("active");
+    $(".num59").toggleClass("active");
+    $(".num58").toggleClass("inactive");
+    $(".num59").toggleClass("inactive");
+    });
+
+$(".num15thumb").on("click", function(){
+    $(".num15").toggleClass("active");
+    $(".num16").toggleClass("active");
+    $(".num15").toggleClass("inactive");
+    $(".num16").toggleClass("inactive");
+    });
+
+$(".num47thumb").on("click", function(){
+    $(".num47").toggleClass("active");
+    $(".num48").toggleClass("active");
+    $(".num47").toggleClass("inactive");
+    $(".num48").toggleClass("inactive");
+    });
